c378d341006b26c0c34fe65fc963b254a6db1958
Nguyen Nhu Hieu <5441003+hieumoscow@users.noreply.github.com>
2024-10-21 09:21:01 +0800

    Delete deploy-script.sh

--- deploy-script.sh
+++
@@ -1,8 +0,0 @@
-az login --service-principal -u $AZ_APP_ID -p $AZ_APP_SECRET --tenant $AZ_TENANT_ID
-az account set --subscription $AZ_SUB_ID
-az extension add --name spring
-az spring app deploy \
-    --resource-group $RG_NAME_DEV \
-    --service $ASA_NAME_DEV \
-    --name $APP_NAME \
-    --artifact-path target/$APP_NAME-$APP_VER.jar
